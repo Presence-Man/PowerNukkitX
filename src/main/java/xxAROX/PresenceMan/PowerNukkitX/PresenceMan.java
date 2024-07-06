648e1ff16474c54acf3fb96ed067f9c1af2f0db0
xxAROX <arox.email.0@gmail.com>
2024-07-06 22:15:29 +0200

    Added LICENSE shit

--- src/main/java/xxAROX/PresenceMan/PowerNukkitX/PresenceMan.java
+++ src/main/java/xxAROX/PresenceMan/PowerNukkitX/PresenceMan.java
@@ -1,6 +1,18 @@
-package xxAROX.PresenceMan.PowerNukkitX;
+/*
+ * Copyright (c) 2024. By Jan-Michael Sohn also known as @xxAROX.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-import cn.nukkit.plugin.PluginBase;
+package xxAROX.PresenceMan.PowerNukkitX;
 
 import cn.nukkit.Player;
 import cn.nukkit.Server;
@@ -10,7 +22,6 @@ import cn.nukkit.utils.Config;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import lombok.NonNull;
-import xxAROX.PresenceMan.PowerNukkitX.EventListener;
 import xxAROX.PresenceMan.PowerNukkitX.entity.ActivityType;
 import xxAROX.PresenceMan.PowerNukkitX.entity.ApiActivity;
 import xxAROX.PresenceMan.PowerNukkitX.entity.ApiRequest;
